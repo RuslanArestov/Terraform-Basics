07560fa3a112ff0a3ea147683e38d7d696cc61e4
debster <debster@Deb.myguest.virtualbox.org>
2025-01-20 10:57:01 +1200

    The tflint and chekov scanner have been checked

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
-variable "default_zone" {
-  type        = string
-  default     = "ru-central1-a"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
-}
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-}
+# variable "default_zone" {
+#   type        = string
+#   default     = "ru-central1-a"
+#   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+# }
+# variable "default_cidr" {
+#   type        = list(string)
+#   default     = ["10.0.1.0/24"]
+#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+# }
 
-variable "vpc_name" {
-  type        = string
-  default     = "develop"
-  description = "VPC network&subnet name"
-}
+# variable "vpc_name" {
+#   type        = string
+#   default     = "develop"
+#   description = "VPC network&subnet name"
+# }
 
 variable "folder_id" {
   type        = string
@@ -21,82 +21,82 @@ variable "folder_id" {
 
 ###common vars
 
-variable "first-vm_name" {
-  type        = string
-  default     = "first-vm"
-}
+# variable "first-vm_name" {
+#   type        = string
+#   default     = "first-vm"
+# }
 
-variable "second-vm_name" {
-  type        = string
-  default     = "second-vm"
-}
+# variable "second-vm_name" {
+#   type        = string
+#   default     = "second-vm"
+# }
 
-variable "image_family" {
-  type        = string
-  default     = "ubuntu-2004-lts"
-}
+# variable "image_family" {
+#   type        = string
+#   default     = "ubuntu-2004-lts"
+# }
 
-variable "public_ip" {
-  type    = bool
-  default = true
-}
+# variable "public_ip" {
+#   type    = bool
+#   default = true
+# }
   
-variable "owner_name" {
-  type        = string
-  default     = "r.arestov"
-}
+# variable "owner_name" {
+#   type        = string
+#   default     = "r.arestov"
+# }
 
-variable "project_first_name" {
-  type        = string
-  default     = "marketing"
-}
+# variable "project_first_name" {
+#   type        = string
+#   default     = "marketing"
+# }
 
-variable "project_second_name" {
-  type        = string
-  default     = "analytic"
-}
+# variable "project_second_name" {
+#   type        = string
+#   default     = "analytic"
+# }
 
-variable "username" {
-  type    = string
-  default = "user_admin"
-}
+# variable "username" {
+#   type    = string
+#   default = "user_admin"
+# }
 
-variable "ssh_public_key" {
-  type    = string
-  default = "~/.ssh/terraform_study.pub"
-}
+# variable "ssh_public_key" {
+#   type    = string
+#   default = "~/.ssh/terraform_study.pub"
+# }
 
-variable "packages" {
-  type    = string
-  default = "nginx"
-}
+# variable "packages" {
+#   type    = string
+#   default = "nginx"
+# }
 
-locals {
-  subnet_zones = [var.default_zone]
-}
+# locals {
+#   subnet_zones = [var.default_zone]
+# }
 
-variable "vpc_name_stage" {
-  type        = string
-  default     = "stage"
-  description = "VPC network&subnet name"
-}
+# variable "vpc_name_stage" {
+#   type        = string
+#   default     = "stage"
+#   description = "VPC network&subnet name"
+# }
 
-variable "stage_cidr" {
-  type        = list(string)
-  default     = ["10.0.2.0/24"]
-}
+# variable "stage_cidr" {
+#   type        = list(string)
+#   default     = ["10.0.2.0/24"]
+# }
 
-variable "zone_and_cidr" {
-    type = list(object({
-    zone = string
-    cidr = string
-  }))
-  default = [
-    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
-    { zone = "ru-central1-d", cidr = "10.0.3.0/24" },
-  ]
-}
+# variable "zone_and_cidr" {
+#     type = list(object({
+#     zone = string
+#     cidr = string
+#   }))
+#   default = [
+#     { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+#     { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+#     { zone = "ru-central1-d", cidr = "10.0.3.0/24" },
+#   ]
+# }
 
 
 # #Токен для Vault
@@ -135,29 +135,29 @@ variable "ydb_database_name" {
   default     = "tfstate-lock-db"
 }
 
-variable "ydb_table_name" {
-  description = "Имя таблицы YDB"
-  type        = string
-  default     = "tfstate_lock_table"
-}
+# variable "ydb_table_name" {
+#   description = "Имя таблицы YDB"
+#   type        = string
+#   default     = "tfstate_lock_table"
+# }
 
-variable "ydb_column_name" {
-  description = "Имя колонки в таблице YDB"
-  type        = string
-  default     = "LockID"
-}
+# variable "ydb_column_name" {
+#   description = "Имя колонки в таблице YDB"
+#   type        = string
+#   default     = "LockID"
+# }
 
-variable "ydb_column_type" {
-  description = "Тип колонки в таблице YDB"
-  type        = string
-  default     = "string"
-}
+# variable "ydb_column_type" {
+#   description = "Тип колонки в таблице YDB"
+#   type        = string
+#   default     = "string"
+# }
 
-variable "ydb_primary_key" {
-  description = "Первичный ключ таблицы YDB"
-  type        = list(string)
-  default     = ["LockID"]
-}
+# variable "ydb_primary_key" {
+#   description = "Первичный ключ таблицы YDB"
+#   type        = list(string)
+#   default     = ["LockID"]
+# }
 
 variable "deletion_protection" {
   type        = bool
@@ -170,11 +170,11 @@ variable "storage_size_limit" {
   default     = 1
 }
 
-variable "not_null" {
-  description = "Не нулевое значение"
-  type        = bool
-  default     = true
-}
+# variable "not_null" {
+#   description = "Не нулевое значение"
+#   type        = bool
+#   default     = true
+# }
 
 variable "entry_for_access_key" {
   type        = string
