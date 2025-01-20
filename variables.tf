549784cb5b909a94321b708d731114a0a985b1ca
debster <debster@Deb.myguest.virtualbox.org>
2025-01-20 15:49:30 +1200

    Add vaildation variables

--- variables.tf
+++ variables.tf
@@ -213,10 +213,10 @@ variable "ip_address" {
   description = "ip-адрес"
   type        = string
   default     = "192.168.0.1"
-  #deafult = "1920.1680.0.1"
+  #default = "1920.1680.0.1"
 
   validation {
-    condition     = can(regex("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", var.ip_address)) && length(split(".", var.ip_address)) == 4 && alltrue([for part in split(".", var.ip_address) : tonumber(part) >= 0 && tonumber(part) <= 255])
+    condition     = can(cidrhost("${var.ip_address}/32", 0))
     error_message = "Значение IP-адреса должно быть корректным"
   }
 }
@@ -224,14 +224,14 @@ variable "ip_address" {
 variable "pool_of_ip_addresses" {
   description = "список ip-адресов"
   type        = list(string)
-  default     = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
-  #default = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
-  
-   validation {
+  default     =  ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+  #default     = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
+
+  validation {
     condition = alltrue([
       for ip in var.pool_of_ip_addresses :
-      can(regex("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", ip)) && length(split(".", ip)) == 4 && alltrue([for part in split(".", ip) : tonumber(part) >= 0 && tonumber(part) <= 255])
+      can(cidrhost("${ip}/32", 0))
     ])
     error_message = "Одно из значений неверное"
   }
-} 
+}
